3feda4da7ff386c533176bc8a75e5fc1c0a293b6
Juan Manuel Tamayo Monje <jmtm141097@gmail.com>
2022-11-09 20:39:54 -0500

    add decorator auth for by used for protect routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, UseGuards, SetMetadata } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { AuthService } from './auth.service'
-import { RawHeaders, GetUser, RoleProtected } from './decorators'
+import { RawHeaders, GetUser, RoleProtected, Auth } from './decorators'
 import { CreateUserDto, LoginUserDto } from './dto'
 import { User } from './entities/user.entity'
 import { UserRoleGuard } from './guards/user-role.guard'
@@ -31,12 +31,21 @@ export class AuthController {
             melo
         }
     }
-    
+
     // @SetMetadata('roles', ['admin', 'super-user'])
     @Get('private2')
     @RoleProtected(ValidRoles.superUser)
     @UseGuards(AuthGuard(), UserRoleGuard)
     privateRoute2(@GetUser() user: User) {
+        return {
+            ok: true,
+            user
+        }
+    }
+
+    @Get('private3')
+    @Auth(ValidRoles.admin)
+    privateRoute3(@GetUser() user: User) {
         return {
             ok: true,
             user
--- src/auth/decorators/index.ts
+++ src/auth/decorators/index.ts
@@ -1,3 +1,4 @@
+export { Auth } from './auth.decorator'
 export { RoleProtected } from './role-protected.decorator'
 export { RawHeaders } from './get-raw-headers.decorator'
 export { GetUser } from './get-user.decorator'
