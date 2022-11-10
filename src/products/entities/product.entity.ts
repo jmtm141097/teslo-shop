a68ae988bd62d4c564ee7541e7f89f4e204c7e86
Juan Manuel Tamayo Monje <jmtm141097@gmail.com>
2022-11-10 06:52:07 -0500

    add relation of user and product

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
+import { User } from 'src/auth/entities/user.entity'
+import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
 import { ProductImage } from './product-image.entity'
 
 @Entity({ name: 'products' })
@@ -52,6 +53,9 @@ export class Product {
 
     @OneToMany(() => ProductImage, (productImage) => productImage.product, { cascade: true })
     images?: ProductImage[]
+
+    @ManyToOne(() => User, (user) => user.product, { eager: true })
+    user: User
 
     @BeforeInsert()
     checkSlugInsert() {
