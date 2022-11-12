dede923ac6cbeaa7a27187a8bf6f11501438fbb9
Juan Manuel Tamayo Monje <jmtm141097@gmail.com>
2022-11-11 22:43:33 -0500

    documentando entities

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
+import { ApiTags } from '@nestjs/swagger'
 import { AuthService } from './auth.service'
 import { RawHeaders, GetUser, RoleProtected, Auth } from './decorators'
 import { CreateUserDto, LoginUserDto } from './dto'
@@ -7,6 +8,7 @@ import { User } from './entities/user.entity'
 import { UserRoleGuard } from './guards/user-role.guard'
 import { ValidRoles } from './interfaces'
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,49 +1,94 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { User } from 'src/auth/entities/user.entity'
 import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
 import { ProductImage } from './product-image.entity'
 
 @Entity({ name: 'products' })
 export class Product {
+    @ApiProperty({
+        example: '0ac12363-e809-48e4-beac-374455b3fc6f',
+        description: 'Product ID',
+        uniqueItems: true
+    })
     @PrimaryGeneratedColumn('uuid')
     id: string
 
+    @ApiProperty({
+        example: 'T-Shirt Teslo',
+        description: 'Product title',
+        uniqueItems: true
+    })
     @Column('text', {
         unique: true
     })
     title: string
 
+    @ApiProperty({
+        example: 0,
+        description: 'Product price'
+    })
     @Column('float', { default: 0 })
     price: number
 
+    @ApiProperty({
+        example: 'Lorem ipsum',
+        description: 'Product description',
+        default: null
+    })
     @Column({
         type: 'text',
         nullable: true
     })
     description: string
 
+    @ApiProperty({
+        example: 't_shirt_teslo',
+        description: 'Product SLUG - for SEO',
+        uniqueItems: true
+    })
     @Column({
         type: 'text',
         unique: true
     })
     slug: string
 
+    @ApiProperty({
+        example: 10,
+        description: 'Product stock',
+        default: 0
+    })
     @Column({
         type: 'int',
         default: 0
     })
     stock: number
 
+    @ApiProperty({
+        example: ['M', 'XL', 'XXL'],
+        description: 'Product sizes',
+        uniqueItems: true
+    })
     @Column({
         type: 'text',
         array: true
     })
     sizes: string[]
 
+    @ApiProperty({
+        example: 'women',
+        description: 'Product gender',
+        uniqueItems: true
+    })
     @Column({
         type: 'text'
     })
     gender: string
 
+    @ApiProperty({
+        example: 'seo',
+        description: 'Product tags',
+        uniqueItems: true
+    })
     @Column({
         type: 'text',
         array: true,
@@ -51,6 +96,7 @@ export class Product {
     })
     tags: string[]
 
+    @ApiProperty()
     @OneToMany(() => ProductImage, (productImage) => productImage.product, { cascade: true })
     images?: ProductImage[]
 
