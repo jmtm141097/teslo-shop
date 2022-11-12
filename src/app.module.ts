066462f3b10a89903c965c7e6c3b5a9ed7efa2be
Juan Manuel Tamayo Monje <jmtm141097@gmail.com>
2022-11-12 11:33:11 -0500

    implementacion se servicios websokects

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { FilesModule } from './files/files.module'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { join } from 'path'
 import { AuthModule } from './auth/auth.module';
+import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
 @Module({
     imports: [
@@ -36,7 +37,9 @@ import { AuthModule } from './auth/auth.module';
 
         FilesModule,
 
-        AuthModule
+        AuthModule,
+
+        MessagesWsModule
     ]
 })
 export class AppModule {}
