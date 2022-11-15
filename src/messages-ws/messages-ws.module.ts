c2041ad0c9e078cf65e49afed5125cd67344f761
Juan Manuel Tamayo Monje <jmtm141097@gmail.com>
2022-11-15 16:12:09 -0500

    implementando las interfaces conexion y desconexion

--- src/messages-ws/messages-ws.module.ts
+++ src/messages-ws/messages-ws.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { MessagesWsService } from './messages-ws.service';
-import { MessagesWsGateway } from './messages-ws.gateway';
+import { Module } from '@nestjs/common'
+import { MessagesWsService } from './messages-ws.service'
+import { MessagesWsGateway } from './messages-ws.gateway'
 
 @Module({
-  providers: [MessagesWsGateway, MessagesWsService]
+    providers: [MessagesWsGateway, MessagesWsService]
 })
 export class MessagesWsModule {}
