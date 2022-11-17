ce83ce3bb48f0eb7cb433d68cacf5ef4e2cedaa0
Juan Manuel Tamayo Monje <jmtm141097@gmail.com>
2022-11-16 22:01:16 -0500

    registrando clientes socket y conectados

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -2,15 +2,16 @@ import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nes
 import { Socket } from 'socket.io'
 import { MessagesWsService } from './messages-ws.service'
 
-@WebSocketGateway({ cors: true, namespace: '/' })
+@WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
     constructor(private readonly messagesWsService: MessagesWsService) {}
 
     handleConnection(client: Socket) {
-        console.log('Cliente conectado: ', client.id)
+        this.messagesWsService.registerClient(client)
+        console.log({ conectados: this.messagesWsService.getConnectedClients() })
     }
 
     handleDisconnect(client: Socket) {
-        console.log('Cliente desconectado: ', client.id)
+        this.messagesWsService.removeClient(client.id)
     }
 }
